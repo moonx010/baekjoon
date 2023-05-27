3496a4e3f8868bba4e062baa75d45f44a313313c
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-05-27 21:17:55 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         ";
+}
