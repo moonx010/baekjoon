2f0fc13c8c452ada0bb498374154fb56d60b9111
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-05-27 21:15:23 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+  scanf("%d %d", &a, &b);
+  while (a != 0 || b != 0)
+  {
+    if (a > b)
+      printf("Yes\n");
+    else
+      printf("No\n");
+    scanf("%d %d", &a, &b);
+  }
+  return 0;
+}
