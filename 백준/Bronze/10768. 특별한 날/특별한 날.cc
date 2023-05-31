48538fc690f16c82b8c4fa9a7997085043031b13
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-01 08:44:04 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int m, d;
+  cin >> m >> d;
+  if (m < 2)
+    cout << "Before";
+  else if (m > 2)
+    cout << "After";
+  else
+  {
+    if (d > 18)
+      cout << "After";
+    else if (d < 18)
+      cout << "Before";
+    else
+      cout << "Special";
+  }
+}
