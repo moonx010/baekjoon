892d57621ecabf0d2f9a1ab8a1858a9aa0edf057
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-05 08:43:45 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  long long int a, b;
+  cin >> a >> b;
+  cout << a * b / 2;
+}
