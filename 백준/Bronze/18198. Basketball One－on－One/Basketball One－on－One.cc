c441a7d71a573d50aaca802dbf1eb3d98f67b3e4
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-06 14:46:14 +0900

    [Bronze IV] Title: Basketball One-on-One, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  string s;
+  cin >> s;
+  cout << s[s.size() - 2];
+}
