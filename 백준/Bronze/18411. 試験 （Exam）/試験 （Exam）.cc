a05a765af46a64a5d05715843fef2be70c0fd571
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-06 15:03:49 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << a + b + c - min(a, min(b, c));
+}
