5132520183fdd1a4511b412a3ad519804144bc24
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-09 23:59:04 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  int n, a, b;
+  cin >> n >> a >> b;
+  cout << min(n, a / 2 + b);
+}
