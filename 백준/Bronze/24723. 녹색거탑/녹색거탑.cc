10cc7061e082c648d046b3eac7d9df7104a3267f
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-09 10:23:00 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a;
+  cin >> a;
+  cout << pow(2, a);
+}
