536e95346f4668ef1eda0c52a854b1a166abfc38
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-15 20:20:02 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  int cur = 0, max = 0;
+  for(int i = 0; i < 10; i++) {
+    int a, b;
+    cin >> a >> b;
+    cur += b - a;
+    if(cur > max) max = cur;
+  }
+  cout << max;
+}
