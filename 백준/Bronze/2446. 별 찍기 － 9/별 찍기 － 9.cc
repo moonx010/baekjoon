aa58b9a4433b0adcc6d7b409a9f4a023c08926b1
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-15 20:15:56 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    for(int j = 0; j < 2 * (n - i) - 1; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  for(int i = n - 2; i >= 0; i--) {
+    for(int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    for(int j = 0; j < 2 * (n - i) - 1; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+}
