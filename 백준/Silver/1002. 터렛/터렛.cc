a2cabe7382c294ed614336adafd6051ec0ba8a63
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-06-25 23:58:47 +0900

    [Silver III] Title: 터렛, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.cc
+++ 백준/Silver/1002. 터렛/터렛.cc
@@ -1,31 +1,28 @@
 #include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    int ans[n];
-    for(int i = 0; i < n; i++) {
-        int x1, y1, r1, x2, y2, r2;
-        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
-        if(x1 == x2 && y1 == y2 && r1 == r2) {
-            ans[i] = -1;
-            continue;
-        }
-        int d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-        int r_sum = r1 + r2;
-        int r_sum_square = r_sum * r_sum;
-        int r_diff = r1 > r2 ? r1 - r2 : r2 - r1;
-        int r_diff_square = r_diff * r_diff;
-        if(r_sum_square < d) ans[i] = 0;
-        else if(r_sum_square > d) {
-            if(r_diff_square > d) ans[i] = 0;
-            else if(r_diff_square < d) ans[i] = 2;
-            else ans[i] = 1;
-        }
-        else ans[i] = 1;
+  ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  int t;
+  cin >> t;
+  for(int i = 0; i < t; i++) {
+    double x1, y1, r1, x2, y2, r2;
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+    double d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    if(d == 0) {
+      if(r1 == r2) cout << -1;
+      else cout << 0;
     }
-
-    for(int i = 0; i < n; i++) {
-        printf("%d\n", ans[i]);
-    }
+    else if (max(r1, r2) - min(r1, r2) > d) cout << 0;
+    else if (max(r1, r2) - min(r1, r2) == d) cout << 1;
+    else if(d > r1 + r2) cout << 0;
+    else if(d == r1 + r2) cout << 1;
+    else cout << 2;
+    cout << "\n";
+  }
 }
