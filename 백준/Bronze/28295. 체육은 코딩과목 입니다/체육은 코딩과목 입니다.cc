f043906765d77bb2dbe77cf54ae202c7cbf33160
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-07-11 09:48:42 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.cc
+++ 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.cc
@@ -1,21 +1,2 @@
 #include <iostream>
-#include <cmath>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  char d[4] = {'N', 'E', 'S', 'W'};
-  int cur = 0;
-  for (int i = 0; i < 10; i++)
-  {
-    int x;
-    cin >> x;
-    cur = (cur + x) % 4;
-  }
-  cout << d[cur];
-}
+using namespace std;int main(){char d[4]={'N','E','S','W'};int c=0,i,x;for(i=0;i<10;i++){cin>>x;c=(c+x)%4;}cout<<d[c];}
