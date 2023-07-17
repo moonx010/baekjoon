b15d568ca60ecc35acd002c83e93bab8f59f75e6
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-07-17 22:44:39 +0900

    [Bronze III] Title: Sum of Odd Sequence, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9713. Sum of Odd Sequence/Sum of Odd Sequence.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    cout << pow((x + 1) / 2, 2) << "\n";
+  }
+}
