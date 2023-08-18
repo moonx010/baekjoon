e9e79de083f5b78d3e0c0b54975f6984f81edb0e
문석훈 <78538108+moonx010@users.noreply.github.com>
2023-08-18 21:19:29 +0900

    [Bronze III] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    cout << 2 + b - a << "\n";
+  }
+}
