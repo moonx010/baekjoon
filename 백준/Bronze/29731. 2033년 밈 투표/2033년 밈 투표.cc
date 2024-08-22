8e29deed87d940a55137a488b7b76a9450ca4923
문석훈 <78538108+moonx010@users.noreply.github.com>
2024-08-22 23:21:24 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<string> rick_astley_promises;
+    rick_astley_promises.push_back("Never gonna give you up");
+    rick_astley_promises.push_back("Never gonna let you down");
+    rick_astley_promises.push_back("Never gonna run around and desert you");
+    rick_astley_promises.push_back("Never gonna make you cry");
+    rick_astley_promises.push_back("Never gonna say goodbye");
+    rick_astley_promises.push_back("Never gonna tell a lie and hurt you");
+    rick_astley_promises.push_back("Never gonna stop");
+
+    int N;
+    cin >> N;
+    cin.ignore(); // 개행 문자 무시
+
+    bool changed = false;
+
+    for (int i = 0; i < N; ++i) {
+        string S;
+        getline(cin, S);
+
+        // S가 공약 목록에 존재하는지 확인
+        if (find(rick_astley_promises.begin(), rick_astley_promises.end(), S) == rick_astley_promises.end()) {
+            changed = true;
+            break;
+        }
+    }
+
+    if (changed) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
